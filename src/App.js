874d885d822e0a10a6e24e855e384c02d4573b88
Rosemary <59313516+Nwosu-Ihueze@users.noreply.github.com>
2020-06-22 12:21:51 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Table from 'react-bootstrap/lib/Table';
+import Image from 'react-bootstrap/lib/Image';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App  extends Component{
+  state = {
+
+  }
+  //
+  getleaderboardData(url, stateName) {
+    axios.get(url)
+    .then(({ data }) => {
+      this.ListeningStateChangedEvent({ [stateName]: data});
+      console.log(this.state);
+    })
+  }
+  componentDidMount(){
+    this.getleaderboardData('');
+
+  }
+  render(){
+    const{} = this.state;
+    return(
+      <div className='App'>
+        <Table stripped condensed hover bordered className="coloBlack"> 
+     <thead>
+       <tr>
+         <th>#</th>
+         <th>Name</th>
+         <th>Slack Name</th>
+         <th>Email</th>
+         <th>Points</th>
+       </tr>
+     </thead>
+     <tbody>
+       {first3.map((row, index)=> {
+         <tr key={row.slackname}>
+           <td>{index + 1}</td>
+           <td>{index + 1}</td>
+           <image src = {row.img} className="imageheight" circle/>
+           <td>{index + 1}</td>
+           <td>{index + 1}</td>
+           <td>{index + 1}</td>
+         </tr>
+       })}
+     </tbody>
+        </Table>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
